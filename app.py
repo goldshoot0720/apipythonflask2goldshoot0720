b1c65c0cce4281b7448bb2f890ad63d75b216b34
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-21 12:54:24 +0800

    20250721 v1

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+import os
+from supabase import create_client, Client
+
+app = Flask(__name__)
+
+url: str = "https://lgztvgybalhvppkfpwdc.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnenR2Z3liYWxodnBwa2Zwd2RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNDkwMTQsImV4cCI6MjA1OTkyNTAxNH0.JfB6J38LmdlvUwIgkdRmQcBDnv6OzFaA-D27S0ylVnA"
+supabase: Client = create_client(url, key)
+@app.route('/')
+def route_root():
+    return ''
+
+@app.get("/mail")
+def get_mail():
+    response = (
+        supabase.table("mail")
+        .select("*")
+        .execute()
+    )
+    return response.data
+
+@app.get("/inventory")
+def get_inventory():
+    response = (
+        supabase.table("inventory")
+        .select("*")
+        .execute()
+    )
+    return response.data
+
+@app.get("/subscription")
+def get_subscription():
+    response = (
+        supabase.table("subscription")
+        .select("*")
+        .execute()
+    )
+    return response.data
---
+++ uploadtogit.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\Documents\api\python\flask2"
+git init
+git add .
+git commit -m "20250721 v1"
+git remote add origin https://github.com/goldshoot0720/apipythonflask2goldshoot0720.git
+git checkout -b master
+git push -u origin master --force
+pause
