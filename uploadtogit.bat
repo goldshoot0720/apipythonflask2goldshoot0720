686107466fd5b6776fdebe5a9322a91e8202a46b
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-21 13:01:14 +0800

    20250721 v2

--- uploadtogit.bat
+++ uploadtogit.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\api\python\flask2"
 git init
 git add .
-git commit -m "20250721 v1"
+git commit -m "20250721 v2"
 git remote add origin https://github.com/goldshoot0720/apipythonflask2goldshoot0720.git
 git checkout -b master
 git push -u origin master --force
